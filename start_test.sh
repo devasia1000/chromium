1170358c9b93d475777c70f95528411177bca8a5
Devasia Manuel <devasia1000@gmail.com>
2013-06-22 11:48:09 -0400

    Added looping support

--- start_test.sh
+++ start_test.sh
@@ -1,36 +1,48 @@
 #!/bin/sh
 
 #These variables are user-editable
-bandwidth=2500
-delay=50
+bandwidth=2500 #in kilobits per second
+delay=50 #in milli-seconds
+iterations=10
+videoPlayTime=180 #in seconds
 
-#Load 'dummynet' kernel module
-sudo insmod ./ipfw3-2012/kipfw-mod/ipfw_mod.ko
+#These variables are NOT user-editable
+i=0;
 
-#Undo bandwidth throttling from last run
-echo "y" | sudo ./ipfw3-2012/ipfw/ipfw flush
+until [  $i -lt $iterations ]; do
 
-#Start bandwidth throttling
-sudo ./ipfw3-2012/ipfw/ipfw add pipe 2 in proto tcp
-sudo ./ipfw3-2012/ipfw/ipfw pipe 2 config bw ${bandwidth}Kbit/s delay ${delay}ms
+	#Load 'dummynet' kernel module
+	sudo insmod ./ipfw3-2012/kipfw-mod/ipfw_mod.ko
 
-#Delete all Chromium cache files
-sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*
-sudo rm -rfv ~/.cache/chromium/Default/Cache/*
+	#Undo bandwidth throttling from last run
+	echo "y" | sudo ./ipfw3-2012/ipfw/ipfw flush
 
-#Delete all Google Chrome cache files - just to be safe
-sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*
-sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*
+	#Start bandwidth throttling
+	sudo ./ipfw3-2012/ipfw/ipfw add pipe 2 in proto tcp
+	sudo ./ipfw3-2012/ipfw/ipfw pipe 2 config bw ${bandwidth}Kbit/s delay ${delay}ms
 
-#Start chromium video
-./out/Release/chrome http://skype-alpha.csail.mit.edu/start.html | tee ~/Desktop/src/chromium.txt &
-#Wait for video to play a while
-sleep 1800
-#Kill chromium
-sudo killall chrome
+	#Delete all Chromium cache files
+	sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*
+	sudo rm -rfv ~/.cache/chromium/Default/Cache/*
 
-#Start ChromiumPostProcessor
-java -jar ChromiumPostProcessor/dist/ChromiumPostProcessor.jar $bandwidth $delay
+	#Delete all Google Chrome cache files - just to be safe
+	sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*
+	sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*
 
-#Undo bandwidth throttling
-echo "y" | sudo ./ipfw3-2012/ipfw/ipfw flush
+	#Start chromium video
+	./out/Release/chrome http://skype-alpha.csail.mit.edu/start.html | tee ~/Desktop/src/chromium.txt &
+	#Wait for video to play a while
+	sleep $videoPlayTime
+	#Kill chromium
+	sudo killall chrome
+
+	#Start ChromiumPostProcessor
+	java -jar ChromiumPostProcessor/dist/ChromiumPostProcessor.jar $bandwidth $delay
+
+	#Undo bandwidth throttling
+	echo "y" | sudo ./ipfw3-2012/ipfw/ipfw flush
+
+	#Increment counter 
+	let i+=1;
+
+done
