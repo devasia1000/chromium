1ba2b1f7bad46dbb12fa59640dfdaa977146e652
Devasia Manuel <devasia1000@gmail.com>
2013-07-03 14:02:58 -0400

    Dummnynet doesnt work, switched back to trickle

--- start_test.sh
+++ start_test.sh
@@ -15,18 +15,11 @@ i=0
 	#Delete all Google Chrome cache files - just to be safe
 	sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*
 	sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*
-	
-	#Start bandwidth throttling
-	sudo ipfw pipe 2 config bw 160Kbit/s
 
 	#Start Chromium
 	trickle -d 300 -u 1000 ~/Desktop/src/out/Release/chrome http://dash-mse-test.appspot.com/dash-player.html 
 
 	#Wait for video to play a while
         sleep $videoPlayTime
-
-	#echo "Changed bandwidth throttling";	
-	#Change bandwidth throttling
-	#sudo ipfw pipe 2 config bw 1Mbit/s
 	
 	let i=i+1;
