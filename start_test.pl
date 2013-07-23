ee2dab4dbf20a3e8cbe66ed9520641f34b9b684e
Devasia Manuel <devasia1000@gmail.com>
2013-07-23 18:26:42 -0400

    specified full path for process.pl

--- start_test.pl
+++ start_test.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl
 
 $videolink=$ARGV[0];
+print $videolink;
 
 #Delete all Chromium cache files
-print `sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*`;
-print `sudo rm -rfv ~/.cache/chromium/Default/Cache/*`;
+`sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*`;
+`sudo rm -rfv ~/.cache/chromium/Default/Cache/*`;
 
 #Delete all Google Chrome cache files - just to be safe
-print `sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*`;
-print `sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*`;
+`sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*`;
+`sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*`;
 
 #Start Chromium
-print `~/Desktop/src/out/Release/chrome ${videolink} | perl process.pl`;
+print `~/Desktop/src/out/Release/chrome ${videolink} | perl ~/Desktop/src/process.pl`;
