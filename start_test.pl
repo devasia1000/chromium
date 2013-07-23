6bb1d02328fb7bcff76fa966d50df6d0cb43a389
Devasia Manuel <devasia1000@gmail.com>
2013-07-23 17:32:54 -0400

    converted startup script to perl

---
+++ start_test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$videolink=$ARGV[0];
+
+#Delete all Chromium cache files
+print `sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*`;
+print `sudo rm -rfv ~/.cache/chromium/Default/Cache/*`;
+
+#Delete all Google Chrome cache files - just to be safe
+print `sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*`;
+print `sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*`;
+
+#Start Chromium
+print `~/Desktop/src/out/Release/chrome ${videolink}`;
