9c82abef9a3938e169412758bb35838bce8596c1
Devasia Manuel <devasia1000@gmail.com>
2013-08-09 18:43:14 -0400

    added post-processing with process.pl

--- start_test.pl
+++ start_test.pl
@@ -2,7 +2,9 @@
 
 $videolink=$ARGV[0];
 
-for($i=0;$i<1000;$i++){
+#uncomment all commented lines to run multiple tests
+
+#for($i=0;$i<1000;$i++){
 #Delete all Chromium cache files
 `sudo rm -rfv ~/.cache/chromium/Default/Media\ Cache/*`;
 `sudo rm -rfv ~/.cache/chromium/Default/Cache/*`;
@@ -12,9 +14,9 @@ for($i=0;$i<1000;$i++){
 `sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*`;
 
 #Start Chromium
-system("~/Desktop/src/out/Release/chrome ${videolink} &");
+print `~/Desktop/src/out/Release/chrome ${videolink} | perl ~/Desktop/src/process.pl`;
 # wait for video to play a while
-sleep(25);
+#sleep(40);
 #send SIGTERM to chromium
-print `sudo killall chrome`;
-}
+#print `sudo killall chrome`;
+#}
