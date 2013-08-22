93f7bd8d07f5a0ef2dd09822157afd9bf40dce6f
Devasia Manuel <devasia1000@gmail.com>
2013-08-22 14:07:01 -0500

    piped chromium output to ssim processing script

--- start_test.pl
+++ start_test.pl
@@ -14,7 +14,7 @@ $videolink=$ARGV[0];
 `sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*`;
 
 #Start Chromium
-print `./out/Release/chrome ${videolink} | perl ~/src/process.pl`;
+print `./out/Release/chrome ${videolink} | python ~/pyssim/youtube_ssim_with_cellsim/ssim_process.py`;
 # wait for video to play a while
 #sleep(40);
 #send SIGTERM to chromium
